a02290b453019b1022238b20f8d83ce49968f31c
Arnau Silberman <arnau.silberman@gmail.com>
2017-12-14 17:09:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/arnausilberman/projectmedusa/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.arnausilberman.projectmedusa;
+
+/**
+ * Created by Arnau Silberman on 30/11/2017.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    // Set the duration of the splash screen
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+}
